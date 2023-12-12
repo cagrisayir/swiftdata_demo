d5628b92a00897cdcaee57861ec94ec166e6ca72
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-12 15:51:08 +0300

    created a new data

--- swiftdata_demo/ContentView.swift
+++ swiftdata_demo/ContentView.swift
@@ -9,14 +9,17 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var context // reference to the context 
+    @Environment(\.modelContext) private var context // reference to the context
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Add") {
+                // Create a new data item
+                let data = DataItem()
+                
+                // Add it to the db
+                context.insert(data)
+            }
         }
         .padding()
     }
