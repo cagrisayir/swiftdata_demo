9bcd4830dabb9dfcd9a54ec0c6a1ecdf82333dc4
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-12 15:44:36 +0300

    creeated persistent container

--- swiftdata_demo/swiftdata_demoApp.swift
+++ swiftdata_demo/swiftdata_demoApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct swiftdata_demoApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: DataItem.self)
         }
     }
 }
