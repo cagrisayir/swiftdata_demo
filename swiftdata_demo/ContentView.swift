f99a34b7dfbe6d80887492aa7a21b9baf2488cb1
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-12 15:54:15 +0300

    retreive data

--- swiftdata_demo/ContentView.swift
+++ swiftdata_demo/ContentView.swift
@@ -11,8 +11,15 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var context // reference to the context
     
+    @Query private var items: Array<DataItem> // auto update
+    // @Query will match items as [DataItem]
+    
     var body: some View {
         VStack {
+            List(items) { item in
+                Text(item.creationDate.description)
+            }
+            
             Button("Add") {
                 // Create a new data item
                 let data = DataItem()
